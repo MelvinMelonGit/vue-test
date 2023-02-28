2e6751ab52132bb9f505c70ca97070b99c3078f5
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2023-02-28 12:38:15 +0800

    Created simple crud app

--- index.js
+++ index.js
@@ -3,20 +3,38 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        title: 'My CRUD App!',
-        textInput: '',
-        data: [
-            {id: 1, subject: 'english'},
-            {id: 2, subject: 'math'},
-            {id: 3, subject: 'science'},
+        title: 'CRUD!',
+        msg: '',
+        task: [
+            {id: 1, subject: 'english', isEditing: false, editMsg: ''},
+            {id: 2, subject: 'math', isEditing: false, editMsg: ''}
         ]
       }
     },
     methods: {
-        addItem() {
-            this.data.push({
-                id: this.data.length +1,
-                subject: this.taskInput
+        addTask() {
+            this.task.push(
+                {id: this.task.length +1, subject: this.msg, isEditing: false, editMsg: ''}
+            )
+            this.msg = ''
+        },
+        deleteTask(id) {
+            this.task = this.task.filter(t=> t.id !== id)
+        },
+        editTask(id) {
+            this.task.forEach(t => {
+                if (t.id === id) {
+                    t.isEditing = !t.isEditing   
+                    t.editMsg = t.subject
+                }
+            })
+        },
+        confirmEdit(id) {
+            this.task.forEach(t => {
+                if (t.id === id) {
+                    t.isEditing = !t.isEditing
+                    t.subject = t.editMsg
+                }
             })
         }
     }
