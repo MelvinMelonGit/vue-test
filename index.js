448efd1f8227dab902660a331efd49c01e9effc7
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2023-02-28 10:38:51 +0800

    additem functionality

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        title: 'My CRUD App!',
+        textInput: '',
+        data: [
+            {id: 1, subject: 'english'},
+            {id: 2, subject: 'math'},
+            {id: 3, subject: 'science'},
+        ]
       }
+    },
+    methods: {
+        addItem() {
+            this.data.push({
+                id: this.data.length +1,
+                subject: this.taskInput
+            })
+        }
     }
   }).mount('#app')
